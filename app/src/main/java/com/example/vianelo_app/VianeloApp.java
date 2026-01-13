99e638092f11c3f8f159783f14950f95ad3e9d19
winded <windedwriter@gmail.com>
2026-01-12 22:29:28 -0700

    try

---
+++ app/src/main/java/com/example/vianelo_app/VianeloApp.java
@@ -0,0 +1,42 @@
+package com.example.vianelo_app;
+
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+
+public class VianeloApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseApp.initializeApp(this);
+
+        boolean isDebug =
+                (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+
+        FirebaseAppCheck appCheck = FirebaseAppCheck.getInstance();
+
+        if (isDebug) {
+            appCheck.installAppCheckProviderFactory(
+                    DebugAppCheckProviderFactory.getInstance()
+            );
+            Log.d("APP_CHECK", "USANDO DebugAppCheckProviderFactory ");
+        } else {
+            appCheck.installAppCheckProviderFactory(
+                    PlayIntegrityAppCheckProviderFactory.getInstance()
+            );
+            Log.d("APP_CHECK", "USANDO PlayIntegrity ");
+        }
+
+
+       // appCheck.getAppCheckToken(false)
+                //.addOnSuccessListener(token -> Log.d("APP_CHECK", "TOKEN OK: " + token.getToken()))
+                //.addOnFailureListener(e -> Log.e("APP_CHECK", "TOKEN FAIL", e));
+    }
+}
